f654ba0af15b6461998967d1fea94460746170ec
Irishmun <steefiscool70@gmail.com>
2021-02-24 15:56:44 +0100

    fixed clock script
    turns out a single space can be a real pain

--- code/clock.js
+++ code/clock.js
@@ -1,24 +1,30 @@
+let today, h, m, clock;
+
+today = new Date();
+h = today.getHours(); //hours
+m = today.getMinutes(); //minutes
+clock = document.getElementById('Clock');
+startTime();
+
 function startTime() {
     //var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     //var days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    let today = new Date();
-    let h = today.getHours(); //hours
-    let m = today.getMinutes(); //minutes
-    let clock = document.getElementById('Clock');
-    //var d = days[(today.getDay() - 1)]; //weekday
-    //var D = today.getDate(); //day of the month
-    //var M = months[(today.getMonth())]; //month
+    today = new Date();
+    h = today.getHours(); //hours
+    m = today.getMinutes(); //minutes
+    clock = document.getElementById('Clock');
     m = checkTime(m);
-    if (clock != null) {
-        if (h >= 0 && h < 11) {
-            clock.innerHTML =
-                h + ":" + m + " AM"; //" - " + d + " " + D + " " + M + " 1997";
-        } else {
-            clock.innerHTML =
-                h + ":" + m + " PM";
-        }
-        let t = setTimeout(startTime, 5000);
+
+    if (h >= 0 && h < 11) {
+        clock.innerHTML =
+            h + ":" + m + " AM"; //" - " + d + " " + D + " " + M + " 1997";
+    } else {
+        clock.innerHTML =
+            h + ":" + m + " PM";
     }
+    let t = setTimeout(startTime, 5000);
+
+
 }
 
 function checkTime(i) {
