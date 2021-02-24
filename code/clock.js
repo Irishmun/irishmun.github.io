480d46b19eb788cb72576c84689cdadd5d6690e1
Irishmun <steefiscool70@gmail.com>
2021-02-24 15:25:08 +0100

    resized welcome message and reworked clock slightly

--- code/clock.js
+++ code/clock.js
@@ -1,21 +1,24 @@
 function startTime() {
     //var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     //var days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    var today = new Date();
-    var h = today.getHours(); //hours
-    var m = today.getMinutes(); //minutes
+    let today = new Date();
+    let h = today.getHours(); //hours
+    let m = today.getMinutes(); //minutes
+    let clock = document.getElementById('Clock');
     //var d = days[(today.getDay() - 1)]; //weekday
     //var D = today.getDate(); //day of the month
     //var M = months[(today.getMonth())]; //month
     m = checkTime(m);
-    if (h >= 0 && h < 11) {
-        document.getElementById('Clock').innerHTML =
-            h + ":" + m + " AM"; //" - " + d + " " + D + " " + M + " 1997";
-    } else {
-        document.getElementById('Clock').innerHTML =
-            h + ":" + m + " PM";
+    if (clock != null) {
+        if (h >= 0 && h < 11) {
+            clock.innerHTML =
+                h + ":" + m + " AM"; //" - " + d + " " + D + " " + M + " 1997";
+        } else {
+            clock.innerHTML =
+                h + ":" + m + " PM";
+        }
+        let t = setTimeout(startTime, 5000);
     }
-    var t = setTimeout(startTime, 5000);
 }
 
 function checkTime(i) {
