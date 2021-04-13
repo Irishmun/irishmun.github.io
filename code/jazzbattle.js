1e12b65cfebfadef46956e17221c1653e63d1336
Irishmun <steefiscool70@gmail.com>
2021-04-13 16:44:17 +0200

    Update jazzbattle.js

--- code/jazzbattle.js
+++ code/jazzbattle.js
@@ -5,15 +5,24 @@ function CalculateAttack() {
     let PreOut = document.getElementById("PreOutput");
     let PosOut = document.getElementById("PosOutput");
     let NegOut = document.getElementById("NegOutput");
+
     AttackerOffense = parseInt(document.getElementById("Off").value);
     Defense = parseInt(document.getElementById("Def").value);
     attackStrength = parseInt(document.getElementById("AType").value);
+
     let Damage = CalcDamage();
-    let PosDamage = Damage + (Damage * damageMod),
-        NegDamage = Damage - (Damage * damageMod);
-    PreOut.innerText = Math.round(Damage) + " (" + Damage + ")";;
-    PosOut.innerText = Math.round(PosDamage) + " (" + PosDamage + ")";
-    NegOut.innerText = Math.round(NegDamage) + " (" + NegDamage + ")";
+    let PosDamage = Damage * 1.25,
+        NegDamage = Damage * 0.75;
+    let RDamage = Math.round(Damage),
+        RPosDamage = Math.round(PosDamage),
+        RNegDamage = Math.round(NegDamage);
+    if (Damage < 0) { RDamage = 0; }
+    if (PosDamage < 0) { RPosDamage = 0; }
+    if (NegDamage < 0) { RNegDamage = 0; }
+
+    PreOut.innerText = RDamage + " (" + Damage + ")";;
+    PosOut.innerText = RPosDamage + " (" + PosDamage + ")";
+    NegOut.innerText = RNegDamage + " (" + NegDamage + ")";
 }
 
 function CalcDamage() {
