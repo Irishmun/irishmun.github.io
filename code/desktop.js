d7da9ca511669cac486a0c2d9a49d228baa34f10
Irishmun <steefiscool70@gmail.com>
2022-02-17 00:25:42 +0100

    one draggable item
    need to make other items also draggable

--- code/desktop.js
+++ code/desktop.js
@@ -1,10 +1,10 @@
 function closeWindow(windowId) {
-    let window = document.getElementById(windowId);
+    let window = document.getElementById(windowId).parentNode;
     window.style.display = "none";
 }
 
 function openWindow(windowId) {
-    let window = document.getElementById(windowId);
+    let window = document.getElementById(windowId).parentNode;
     if (window.style.display == "none") {
         window.style.display = "block";
     }
