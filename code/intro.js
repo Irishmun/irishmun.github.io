460527a71e2351857a58c8ec5e4925da11b7d851
Irishmun <steefiscool70@gmail.com>
2023-01-19 01:00:48 +0100

    new font, added display css
    wow so futuristic
    
    use for actual documents perhaps? (would benefit from disable checkbox that would set it to default black-white)

--- code/intro.js
+++ code/intro.js
@@ -1,23 +1,23 @@
 let header = document.getElementById("Intro");
 let animation = [
-    { t: "[|]", ms: 500 },
-    { t: "[ ]", ms: 500 },
-    { t: "[|]", ms: 500 },
-    { t: "[ ]", ms: 500 },
-    { t: "[|]", ms: 500 },
-    { t: "[S|]", ms: 200 },
-    { t: "[ST|]", ms: 200 },
-    { t: "[STE|]", ms: 200 },
-    { t: "[STEE|]", ms: 200 },
-    { t: "[STEEF|]", ms: 200 },
-    { t: "[STEEF ]", ms: 200 },
-    { t: "[STEEF|]", ms: 500 },
-    { t: "[STEEF ]", ms: 500 },
-    { t: "[STEEF|]", ms: 500 },
-    { t: "[STEEF ]", ms: 500 },
-    { t: "[STEEF|]", ms: 500 },
-    { t: "[STEEF]", ms: 500 },
-    { t: "[STEEF]", ms: 100 },
+    { t: "_", ms: 250 },
+    { t: "&nbsp&nbsp", ms: 250 },
+    { t: "_", ms: 250 },
+    { t: "&nbsp&nbsp", ms: 250 },
+    { t: "_", ms: 250 },
+    { t: "S_", ms: 100 },
+    { t: "ST_", ms: 100 },
+    { t: "STE_", ms: 100 },
+    { t: "STEE_", ms: 100 },
+    { t: "STEEF_", ms: 100 },
+    { t: "STEEF&nbsp&nbsp", ms: 100 },
+    { t: "STEEF_", ms: 250 },
+    { t: "STEEF&nbsp&nbsp", ms: 250 },
+    { t: "STEEF_", ms: 250 },
+    { t: "STEEF&nbsp&nbsp", ms: 250 },
+    { t: "STEEF_", ms: 250 },
+    { t: "STEEF", ms: 250 },
+    { t: "STEEF", ms: 50 },
 ];
 
 let stepDenominator = 1;
@@ -27,7 +27,7 @@ if (window.localStorage.stepDenominator) {
 let i = 0;
 let update = () => {
     let step = animation[i];
-    header.innerText = step.t;
+    header.innerHTML = step.t;
     i++;
     if (i < animation.length) {
         setTimeout(update, step.ms / stepDenominator)
@@ -37,7 +37,7 @@ let update = () => {
             document.getElementById("Main").style.opacity = 1;
             //any extra code here
         }, 500);
-        window.localStorage.stepDenominator = 2;
+        window.localStorage.stepDenominator = 1;
     }
 }
 update();
