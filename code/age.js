f2412e6875c74b7fa737e5aed39ce4bbd5d61b65
Irishmun <steefiscool70@gmail.com>
2024-02-06 00:32:40 +0100

    Update age.js
    now it works, I'm sure of it.

--- code/age.js
+++ code/age.js
@@ -1,12 +1,13 @@
-const birth = 981417600000;
+const birth = new Date(981414000000);
+const MS_PER_YEAR = 1000 * 60 * 60 * 24 * 365;
 const epochYear = 1970;
-let today = Date.now();
-let currentAge = new Date(today - birth);
 let AgeElement = document.getElementById('Age');
 
 function SetAge() {
-    today = Date.now();
-    AgeElement.innerText = Math.abs(currentAge.getUTCFullYear() - epochYear);
+    let today = new Date();
+    let todayUTC = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate());
+    let currentAge = (todayUTC - birth) / MS_PER_YEAR;
+    AgeElement.innerText = Math.abs(Math.floor(currentAge));
     setTimeout(SetAge, 30000);
 }
 
